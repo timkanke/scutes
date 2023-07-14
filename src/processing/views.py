704435000ec877ee807b159c17c6b19e71884695
Tim Kanke <timkanke@users.noreply.github.com>
2023-07-14 15:19:24 -0400

    LIBWHCA-105. Add template pages

--- src/processing/views.py
+++ src/processing/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class Dashboard(TemplateView):
+    template_name = "dashboard.html"
+
+
+class BatchList(TemplateView):
+    template_name = "batch_list.html"
+
+
+class ItemList(TemplateView):
+    template_name = "item_list.html"
+
+
+class ItemView(TemplateView):
+    template_name = "item_view.html"
