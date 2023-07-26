c44fffdee5933ea90cd13c85a772e56b5cffb49d
Tim Kanke <timkanke@users.noreply.github.com>
2023-07-26 11:07:29 -0400

    LIBWHCA-108. Refactored form layout

--- src/processing/forms.py
+++ src/processing/forms.py
@@ -1,32 +1,31 @@
-from django import forms
-
+from django.forms import ModelForm, TextInput, Textarea
 from django_ckeditor_5.widgets import CKEditor5Widget
 from .models import Item
 
 
-class ItemUpdateForm(forms.ModelForm):
+class ItemUpdateForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # it is required to set it False,
-        # otherwise it will throw error in console
-        self.fields["comment"].required = False
-        self.fields["body_redact"].required = False
+        # fields that are null=true
+        self.fields['comment'].required = False
+        self.fields['body_redact'].required = False
 
     class Meta:
         model = Item
         fields = ('reporter',
                   'title',
+                  'body_redact',
                   'pool_report',
                   'publish',
                   'off_the_record',
                   'review_status',
-                  'comment',
-                  'body_redact')
+                  'comment')
 
-        # you can use CKEditor5Widget in forms or models.
         widgets = {
-            "body_redact": CKEditor5Widget(
-                attrs={"class": "django_ckeditor_5"}, config_name="extends"
+            'reporter': TextInput(attrs={'class': 'form-control'}),
+            'comment': Textarea(attrs={'class': 'form-control'}),
+            'body_redact': CKEditor5Widget(
+                attrs={'class': 'django_ckeditor_5'}, config_name='extends'
             )
         }
