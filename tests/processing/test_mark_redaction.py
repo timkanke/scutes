016e9381fd0abf57efac5bb24ce70e3586905d8d
Tim Kanke <timkanke@users.noreply.github.com>
2023-08-10 13:33:49 -0400

    LIBWHCA-114. Added mark_redaction tests

---
+++ tests/processing/test_mark_redaction.py
@@ -0,0 +1,33 @@
+import re
+
+import pytest
+
+from src.processing.management.commands.mark_redaction import redact_using_patterns, redact_using_string
+
+
+# TODO Requires test database setup
+@pytest.mark.django_db(True)
+@pytest.mark.parametrize(
+    ('value', 'expected'),
+    [
+        ('<p>eggs</p>',
+         '<p><del class="redacted" style="color:red;">eggs</del></p>'),
+    ]
+)
+def test_redact_using_string(value, expected):
+    # result = redact_using_string(value)
+    # print(result)
+    # assert result == expected
+    pass
+
+
+@pytest.mark.parametrize(
+    ('value', 'expected'),
+    [
+        ('<p>eggs@spam.com</p>',
+         '<p><del class="redacted" style="color:red;">eggs@spam.com</del></p>'),
+    ]
+)
+def test_redacted_using_patterns(value, expected):
+    result = redact_using_patterns(value)
+    assert result == expected
