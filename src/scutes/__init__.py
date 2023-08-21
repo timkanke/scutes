1030332490d98ede86378b1c2183f743cee6ddf3
Tim Kanke <timkanke@users.noreply.github.com>
2023-08-21 09:14:07 -0400

    LIBWHCA-128. Added project.scripts

---
+++ src/scutes/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+
+def django_manage():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scutes.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
