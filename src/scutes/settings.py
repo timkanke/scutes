9ae1b8f8c306c5f495baf882d644f6df974afa80
Tim Kanke <timkanke@users.noreply.github.com>
2023-08-29 13:31:25 -0400

    LIBWHCA-135. Added Logging

--- src/scutes/settings.py
+++ src/scutes/settings.py
@@ -162,6 +162,23 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": env('LOGGING_LEVEL'),
+    },
+}
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
