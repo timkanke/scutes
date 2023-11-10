f08f7b041081673e27ebf8df7e79d3f6a44be239
Tim Kanke <timkanke@users.noreply.github.com>
2023-11-10 15:42:34 -0500

    LIBWHCA-155 Modified item list filters

--- src/processing/filters.py
+++ src/processing/filters.py
@@ -1,11 +1,24 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter, ChoiceFilter
 
 from .models import Item
 
+PUBLISH_CHOICES = (
+    (0, 'False'),
+    (1, 'True'),
+)
+
+STATUS_CHOICES = (
+    (0, 'Not Started'),
+    (1, 'In Progress'),
+    (2, 'Complete'),
+)
+
 
 class ItemFilter(FilterSet):
+    reporter = CharFilter(field_name="reporter", label="Reporter Name Contains",lookup_expr='icontains')
+    publish = ChoiceFilter(field_name="publish", label="Publish", choices=PUBLISH_CHOICES)
+    review_status = ChoiceFilter(field_name="review_status", label="Editorial Review", choices=STATUS_CHOICES)
+
     class Meta:
         model = Item
-        fields = {
-            'reporter': ['icontains'], 'review_status': ['exact']
-            }
+        fields = ('reporter', 'publish', 'review_status')
