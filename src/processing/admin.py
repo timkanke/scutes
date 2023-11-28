3ebc403a83614ddb70eb196d64370c5d0d6349d5
Tim Kanke <timkanke@users.noreply.github.com>
2023-11-28 15:39:09 -0500

    LIBWHCA-121 Added FileAdmin

--- src/processing/admin.py
+++ src/processing/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Batch, Item, Redact
+from .models import Batch, File, Item, Redact
 
 
 @admin.register(Batch)
 class BatchAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['title', 'file']
 
 
 @admin.register(Item)
