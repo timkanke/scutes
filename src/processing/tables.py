00464551a0e97f1cba1446b7b2ab2a4f8eb623bf
Tim Kanke <timkanke@users.noreply.github.com>
2023-12-15 08:56:21 -0500

    LIBWHCA-161 Added attchment and inline count

--- src/processing/tables.py
+++ src/processing/tables.py
@@ -5,11 +5,9 @@ from .models import Batch, Item
 class BatchList(tables.Table):
     class Meta:
         model = Batch
-        template_name = "django_tables2/bootstrap5.html"
+        template_name = 'django_tables2/bootstrap5.html'
         attrs = {'class': 'table table-sm'}
-        fields = ['id',
-                  'name',
-                  'assigned_to']
+        fields = ['id', 'name', 'assigned_to']
 
     review = TemplateColumn(template_name='tables/view_item_list.html', orderable=False)
     assigned_to = Column(attrs={'th': {'class': 'text-decoration-line-through text-muted'}}, orderable=False)
@@ -18,28 +16,38 @@ class BatchList(tables.Table):
 
 
 class ItemList(tables.Table):
-
-    review_status = Column(attrs={'td': {'class': lambda value: 'text-success' if value == 'Complete'
-                                         else ('text-warning' if value == 'In Progress'
-                                               else 'text-danger')}}, orderable=False)
+    review_status = Column(
+        attrs={
+            'td': {
+                'class': lambda value: 'text-success'
+                if value == 'Complete'
+                else ('text-warning' if value == 'In Progress' else 'text-danger')
+            }
+        },
+        orderable=False,
+    )
 
     class Meta:
         model = Item
         template_name = 'django_tables2/bootstrap5.html'
         attrs = {'class': 'table table-sm'}
-        row_attrs = {
-            'review_status': lambda value: value.name
-        }
-        fields = ['id',
-                  'date',
-                  'reporter',
-                  'title',
-                  'publish',
-                  'review_status']
+        row_attrs = {'review_status': lambda value: value.name}
+        fields = [
+            'id',
+            'date',
+            'reporter',
+            'title',
+            'publish',
+            'review_status',
+            'attachment_count',
+            'inline_count',
+        ]
 
     id = Column(verbose_name='ID', orderable=False)
     date = Column(orderable=False)
     reporter = Column(orderable=False)
     title = Column(orderable=False)
     publish = Column(orderable=False)
+    attachment_count = Column(orderable=False)
+    inline_count = Column(orderable=False)
     review = TemplateColumn(template_name='tables/view_item.html', orderable=False)
