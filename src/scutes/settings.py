11cae6474852823da4185d645f61ad4de0060a0f
Tim Kanke <timkanke@users.noreply.github.com>
2024-02-09 08:59:32 -0500

    LIBWHCA-205 Added django-htmx

--- src/scutes/settings.py
+++ src/scutes/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django_filters',
     'django_bootstrap5',
     'django_ckeditor_5',
+    'django_htmx',
     'crispy_forms',
     'crispy_bootstrap5',
     'debug_toolbar',
@@ -71,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
     'django.contrib.admindocs.middleware.XViewMiddleware',
 ]
 
