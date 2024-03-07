820c4dd21fb6b726f1bce0d8857da27284de222d
Tim Kanke <timkanke@users.noreply.github.com>
2024-03-07 15:30:27 -0500

    LIBWHCA-123 Refactor is_authorized

--- src/scutes/authentication.py
+++ src/scutes/authentication.py
@@ -11,10 +11,7 @@ class ModifiedSaml2Backend(Saml2Backend):
         **kwargs,
     ) -> bool:
         groups = [g.lower() for g in attributes['eduPersonEntitlement']]
-        if 'scutes-user' not in groups:
-            return False
-        else:
-            return True
+        return 'scutes-user' in groups
 
     def _update_user(self, user, attributes: dict, attribute_mapping: dict, force_save: bool = False):
         if 'eduPersonEntitlement' in attributes:
