0df927c6a60ce407a7168b1e41d797b982190ae9
Tim Kanke <timkanke@users.noreply.github.com>
2024-03-08 08:57:12 -0500

    LIBWHCA-123 Clean up

--- src/processing/management/commands/import_process.py
+++ src/processing/management/commands/import_process.py
@@ -33,40 +33,24 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('file_path', type=str, help='File path to be imported.')
 
-        # Optional argument
-        parser.add_argument(
-            '--file',
-            type=str,
-            help='Upload one file',
-        )
-
     def handle(self, *args, **options):
-        import_file = options['file']
         file_path = Path(options['file_path'])
         files = []
         existing_batchs = list(Batch.objects.all().values_list('name', flat=True))
         existing_batchs = [existing_batch + '.mbox' for existing_batch in existing_batchs]
 
-        if import_file:
+        for file in file_path.iterdir():
+            if file.suffix == '.mbox':
+                files.append(str(file.name))
+        for i in files[:]:
+            if i in existing_batchs:
+                files.remove(i)
+        files = [str(file_path) + '/' + file for file in files]
+        files.sort()
+        logger.info(f'Files import list: {files}')
+
+        for file_path in files:
+            logger.debug(f'Importing: {file_path}')
             import_batch(file_path)
-        else:
-            # for dirpath, dirnames, filenames in dir.walk():
-            # files.extend(dirname / name for name in filenames if name.endswith(‘.mbox’))
 
-            paths = Path(file_path).glob(
-                '**/*.mbox',
-            )
-            for path in paths:
-                files.append(str(path.name))
-            for i in files[:]:
-                if i in existing_batchs:
-                    files.remove(i)
-            files = [str(file_path) + '/' + file for file in files]
-            files.sort()
-            logger.debug(files)
-
-            for file_path in files:
-                logger.debug(f'Importing: {file_path}')
-                import_batch(file_path)
-
-        logger.debug('Import Complete')
+        logger.info('Import Complete')
