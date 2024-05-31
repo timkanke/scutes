395b8bed62bc91985ec6912d11ea7f9259277bcc
Tim Kanke <timkanke@users.noreply.github.com>
2024-05-31 13:53:56 -0400

    LIBWHCA-149 Added phonenumbers library

--- tests/test_mark_redaction.py
+++ tests/test_mark_redaction.py
@@ -1,26 +1,13 @@
 import pytest
 
 from django.core.management import call_command
-from src.processing.common.mark_redaction import redact_using_pattern
+from src.processing.common.mark_redaction import redact_phonenumbers, redact_using_pattern
 
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command('loaddata', 'test_data.yaml')
-
-
-# @pytest.mark.parametrize(
-#     ('value', 'expected'),
-#     [
-#         ('<p>eggs</p>',
-#          '<p><del class="redacted" style="color:red;">eggs</del></p>'),
-#     ]
-# )
-# def test_redact_using_string(db, django_db_setup, value, expected):
-#     result = redact_using_string(value)
-#     print(result)
-#     assert result == expected
 
 
 @pytest.mark.parametrize(
@@ -32,4 +19,20 @@ def django_db_setup(django_db_setup, django_db_blocker):
 )
 def test_redacted_using_patterns(value, expected):
     result = redact_using_pattern(value)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ('value', 'expected'),
+    [
+        ('<p>515-555-5515</p>',
+         '<p><del class="redacted" style="color:red;">515-555-5515</del></p>'),
+        ('<p>(515)555-5515</p>',
+         '<p><del class="redacted" style="color:red;">(515)555-5515</del></p>'),
+         ('<p>(515) 555-5515</p>',
+         '<p><del class="redacted" style="color:red;">(515) 555-5515</del></p>'),
+    ]
+)
+def test_redacted_phonenumbers(value, expected):
+    result = redact_phonenumbers(value)
     assert result == expected
