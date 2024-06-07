64b1a6afb3649c705f6003659469274fe170d259
Tim Kanke <timkanke@users.noreply.github.com>
2024-06-07 09:39:08 -0400

    LIBWHCA-250 Added humanize

--- src/scutes/settings.py
+++ src/scutes/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.humanize',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'processing',
